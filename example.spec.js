2ebd790851d9a1ff0971c5bb09a491b991bcd72d
aisyahahmadkassim <aisyahahmadkassim@gmail.com>
2026-07-10 11:24:24 +0800

    Add files via upload

---
+++ example.spec.js
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('Search attractions', async ({ page }) => {
+  await page.goto('https://www.booking.com/attractions/index.en-gb.html');
+
+  // Type destination
+  await page.getByTestId('search-input-field').click();
+  await page.keyboard.type('Sunway', { delay: 1000 });
+  await page.getByText(/Sunway/i).first().click();
+  // await page.waitForTimeout(10000);
+  await page.keyboard.press('Enter');
+  // await page.keyboard.press('Enter');
+  // await page.keyboard.press('Enter');
+
+
+
+
+
+
+
+  //date
+  // await page.getByTestId('search-button').click();
+  // await page.getByTestId('search-button').click();
+  // await page.getByTestId('search-button').click();
+  await page.pause();
+
+  //date selection
+
+});
